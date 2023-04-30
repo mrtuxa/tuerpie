cf078c50ed313f98cad12e0d6f1568e85045be83
tuxa <69870860+mrtuxa@users.noreply.github.com>
2023-04-30 18:03:19 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-mkdir app
-cd app || exit
-git clone https://github.com/dezentrale/spaceapi-rs app/spaceapi
-cd app/spaceapi || exit
-nix-shell -p pkg-config openssl --command "cargo build --release"
-cd .. || exit
-cp target/release/spaceapi-dezentrale-client .
-cp ../target/release/pi .
